453a73d35fda3f1361171a80411dc15226b9e1a8
Olcho <dubriduba@gmail.com>
2026-04-08 18:49:14 +0900

    04081

--- package2/Step3.java
+++ package2/Step3.java
@@ -1,5 +1,18 @@
-package package2;
+package package2; // 다른 패키지
+
+import package1.member;
 
 public class Step3 {
-    
+    public static void main(String[] args) {
+        member member = new member("김멋대", "컴퓨터공학과", 14);
+        
+        System.out.println("Step 3-1. public 필드 접근 시도");
+        member.name = "홍길동"; // 성공: public
+        System.out.println("public 필드 접근 성공");
+        member.printInfo();
+    }
+
+        //member.major = "소프트웨어학과";   오류1
+        //member.generation = 15;   오류2
+
 }
--- week1.java
+++ week1.java
@@ -1,36 +1,83 @@
-import java.util.Scanner; //스캐너가져오기
+import java.util.Scanner;
 
 public class week1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int count = 0;
+        Scanner sc = new Scanner(System.in);
 
-        //멤버 수 입력
         while (true) {
-            System.out.println("저장할 멤버 수를 5 이상 입력 : ");
-            count = scanner.nextInt();
-            scanner.nextLine();
+            int count = 0;
+            
+            // 1. 멤버 입력
+            while (true) { // 1불가능 
+                System.out.println("멤버 수를 5 이상 입력하세요 : ");
+                String input = sc.nextLine();
+                try {
+                    count = Integer.parseInt(input);
+                    if (count < 5) {
+                        System.out.println("5 이상 입력하세요 : ");
+                        continue;
+                    }
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("숫자만 입력 가능합니다");
+                }
+            }
 
-            if (count >= 5) {
-                break;
-            } else {
-                System.out.println("[오류] 5 이상 입력해주세요.");
+            // 2. 이름 입력
+            String[] members = new String[count];
+            System.out.println("멤버 이름을 입력하세요 : ");
+            
+            for (int i = 0; i < count; i++) {
+                String name = sc.nextLine();
+
+                if (name.trim().isEmpty()) {
+                    System.out.println("이름 없습니다. 다시 입력하세요 ");
+                    i--;
+                    continue;
+                }
+
+                boolean isDuplicate = false;
+                for (int j = 0; j < i; j++) {
+                    if (name.equals(members[j])) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (isDuplicate) {
+                    System.out.println("중복된 이름입니다, 다시 입력하세요.");
+                    i--;
+                    continue;
+                }
+
+                members[i] = name;
+            }
+
+            // 3. print
+            System.out.println("멤버 출력");
+
+            for (int i = 0; i < members.length; i++) {
+                System.out.println("🦁 " + (i + 1) + ". " + members[i]);
+            }
+
+            // 4. 종료
+            System.out.println("\n======================");
+            System.out.println("프로그램을 종료하려면 exit");
+            System.out.println("계속 멤버를 등록하려면 restart");
+            
+            while (true) {
+                String command = sc.nextLine();
+
+                if (command.equalsIgnoreCase("exit")) {
+                    System.out.println("멤버 명단 관리 프로그램을 종료합니다.");
+                    sc.close();
+                    return;
+                } else if (command.equalsIgnoreCase("restart")) {
+                    break;
+                } else {
+                    System.out.println("다시 입력해주세요.");
+                }
             }
         }
-
-        //멤버이름 입력
-        String[] babyLions = new String[count];
-        System.out.println("멤버 이름을 입력 : ");
-        
-        for (int i = 0; i < count; i++) {
-            babyLions[i] = scanner.nextLine();
-        }
-
-        //print
-        System.out.println("\n 명단을 출력.");
-        for (int i = 0; i < babyLions.length; i++) {
-            System.out.println((i + 1) + ". " + babyLions[i]);
-        }
-        scanner.close();
     }
 }
